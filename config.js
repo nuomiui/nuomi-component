99e32d29b061cf527565230d90d230127a6efbd1
memoryza <jincai.wang@foxmail.com>
2018-01-12 15:33:39 +0800

    delete code

--- config.js
+++ config.js
@@ -3,10 +3,10 @@
  * @author memoryza(jincai.wang@foxmail.com)
  */
 module.exports = {
-    // 发布开发机配置
+    // 发布开发机配置(fisp的配置）
     deploy: [{
-        rdtest:'http://cp01-rdqa04-dev120.cp01.baidu.com:8099/receiver.php',
-        odpdir:'/home/users/wangjincai/odp/webroot'
+        rdtest:'http://xxxx.com/receiver.php',
+        odpdir:'/xxxx/webroot'
     }][0],
     // 域名代理服务
     // 规则:https://github.com/chimurai/http-proxy-middleware
@@ -14,19 +14,19 @@ module.exports = {
         host: '0.0.0.0',
         disableHostCheck: true,
         proxy: [{
-            // 教育频道页面api代理
+            // 页面api代理
             context: ['/gaiya'],
-            target: 'https://chi.nuomi.com',
+            target: 'https://xxx.com',
             headers: {
-                Host: 'chi.nuomi.com',
+                Host: 'xxx.com',
                 'Access-Control-Allow-Origin': '*'
             },
             secure: false
         },{
             context: ['/bizareatop'],
-            target: 'https://chi.nuomi.com',
+            target: 'https://xxx.com',
             headers: {
-                Host: 'chi.nuomi.com',
+                Host: 'xxx.com',
                 'Access-Control-Allow-Origin': '*'
             },
             secure: false
@@ -35,7 +35,7 @@ module.exports = {
     // 项目根地址相对于webserver指定的跟地址路径
     webserverRoot: '',
     // webserverRoot: '/channel/output',
-    //糯米组件平台分散认证域名
+    //组件平台分散认证域名
     domainlist: ['xxx.xxx.com'],
     // 是否开启文件监听和请求参数补全
     audient: true
